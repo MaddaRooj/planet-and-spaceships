0367fb7d478576b88fd626756850369b2542f8e9
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-07-08 21:50:53 -0500

    commented code

--- Program.cs
+++ Program.cs
@@ -8,26 +8,29 @@ namespace csharp_lists
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello World!");
+            Console.WriteLine("Problem 1: ");
+            // Add Jupiter and Saturn to the planetList
             List<string> planetList = new List<string>(){"Mercury", "Mars"};
-            planetList.ForEach(planet => Console.WriteLine(planet));
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
-            Console.WriteLine("-----------------------Break--------------------------");
             planetList.ForEach(planet => Console.WriteLine(planet));
-            Console.WriteLine("-----------------------Break--------------------------");
+            Console.WriteLine("Problem 2 - 3: ");
+            // Combine planetList and planetListOuter
             List<string> planetListOuter = new List<string>(){"Uranus", "Neptune"};
             planetList.AddRange(planetListOuter);
             planetList.ForEach(planet => Console.WriteLine(planet));
-            Console.WriteLine("-----------------------Break--------------------------");
+            Console.WriteLine("Problem 4 - 5: ");
+            // Add Venus, Earth, and Pluto to the planetList
             planetList.Insert(1, "Venus");
             planetList.Insert(2, "Earth");
             planetList.Add("Pluto");
             planetList.ForEach(planet => Console.WriteLine(planet));
-            Console.WriteLine("-----------------------Break--------------------------");
+            Console.WriteLine("Problem 6: ");
+            // create a new list that contains the rocky planets from planetList
             List<string> rockyPlanets = planetList.GetRange(0, 4);
             rockyPlanets.ForEach(planet => Console.WriteLine(planet));
-            Console.WriteLine("-----------------------Break--------------------------");
+            Console.WriteLine("Problem 7: ");
+            // Remove pluto from the list
             planetList.Remove("Pluto");
             planetList.ForEach(planet => Console.WriteLine(planet));
         }
